80555fcb4b5742b7137fa69785f58456de13fe10
sarthakastic <iamsg26@gmail.com>
2023-10-01 10:58:01 +0530

    add : dashboard ui

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { UserButton } from "@clerk/nextjs";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const DashboardPage = (props: Props) => {
+  return (
+    <div className="grainy min-h-screen ">
+      <div className="max-w-7xl mx-auto p-10">
+        <div className="h-14">
+          <div className="flex justify-between items-center md:flex-row flex-col">
+            <div className="flex items-center ">
+              <Link href="/">
+                <Button className="bg-green-600">
+                  <ArrowLeft className="mr-1 h-4 w-4" />
+                  Back
+                </Button>
+              </Link>
+              <div className="w-4"></div>
+              <h1 className="text-3xl font-bold text-gray-900  ">My Notes</h1>
+              <div className="w-4">
+                <UserButton />
+              </div>
+            </div>
+            <div className="h-8">
+              <Separator />
+              <div className="h-8"></div>
+              <div className="text-center">
+                <h2 className="text-xl text-gray-500">You have no notes yet</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
